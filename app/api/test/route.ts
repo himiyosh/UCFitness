6c2e885a4ab6b36d5b613030295f6aae21948767
himiyosh <himiyosh.344@gmail.com>
2026-01-23 06:57:59 +0900

    fix: Truly delete test API route

--- app/api/test/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export const dynamic = 'force-dynamic';
-
-export async function GET(request: Request) {
-    return NextResponse.json({ message: "Test OK" });
-}
