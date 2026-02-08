f92e873dd8cb0e806fa1f86aba31c93053e533f3
himiyosh <--show-origin>
2026-02-09 08:28:01 +0900

    fix: リーダーボード行高さを56px→64pxに拡大し5人目の切れを解消

--- components/AnimatedLeaderboard.tsx
+++ components/AnimatedLeaderboard.tsx
@@ -232,7 +232,7 @@ export default function AnimatedLeaderboard({ userId, allGlobalRankings, allGrou
 
                                             return (
                                                 <div className="flex flex-col h-full">
-                                                    <div className="shrink-0" style={{ height: `${ITEMS_PER_PAGE * 56}px` }}>
+                                                    <div className="shrink-0" style={{ height: `${ITEMS_PER_PAGE * 64}px` }}>
                                                         {paginatedItems.map((entry) => {
 
                                                             return (
