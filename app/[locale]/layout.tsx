25347b29dba62f587d142422c0b2627f9e18f5df
himiyosh <himiyosh.344@gmail.com>
2026-02-11 18:00:33 +0900

    フォントウェイト復元: 800/900を再追加 (ロゴ等のfont-black/font-extraboldに必要)

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -58,7 +58,7 @@ export default async function LocaleLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Inter:wght@700;800&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700;800;900&family=Inter:wght@700;800;900&display=swap" rel="stylesheet" />
       </head>
       <body>
         <NextIntlClientProvider messages={messages}>
